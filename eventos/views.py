5703df1687763290abbc9b4a8e98fce31794f9c6
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-07-13 20:48:21 -0300

    Criando a pagina para adicionar eventos

---
+++ eventos/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def novo_evento(request):
+    if request.method == "GET":
+        return render(request, 'novo_evento.html')
