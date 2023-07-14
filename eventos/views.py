5564fc9b2be2bb159397943eb0e14e07663eb0c5
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-07-14 20:49:21 -0300

    adicionando models evento

--- eventos/views.py
+++ eventos/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
+@login_required # faz com que os usuarios so acessem esta url se estivem logados
 def novo_evento(request):
     if request.method == "GET":
         return render(request, 'novo_evento.html')
