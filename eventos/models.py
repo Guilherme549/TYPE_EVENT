2c236526fce7b6d2b7ff9e923286073a3b08fc75
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-07-15 12:59:47 -0300

    Criando instancia para salvar no banco de dados

--- eventos/models.py
+++ eventos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Evento(models.Model):
+    criador = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
     nome = models.CharField(max_length=200)
     descricao = models.TextField()
     data_inicio = models.DateField()
@@ -14,3 +15,6 @@ class Evento(models.Model):
     cor_principal = models.CharField(max_length=7)
     cor_secundaria = models.CharField(max_length=7)
     cor_fundo = models.CharField(max_length=7)
+
+    def __str__(self) -> str:
+        return self.nome
