3d2c1c0593bcb984127cc4304c1436a22033a50c
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-07-17 23:48:38 -0300

    vercel.json

--- type_event/settings.py
+++ type_event/settings.py
@@ -25,10 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*w5x8d8-f1c=+he2!f999xq=cpnv93*hok+h+pl9+xl$gpxmg_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'type_event.vercel.app']
-
 
 # Application definition
 
@@ -121,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 
